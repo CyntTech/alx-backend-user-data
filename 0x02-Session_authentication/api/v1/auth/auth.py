09b53a02bb2aba77dfd41c12d5e2d89122ce58db
Cynthia Amaji <cynthyok4luv@gmail.com>
2023-03-23 23:11:28 +0000

    session authentication

---
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+"""authentication module"""
+
+from flask import request
+import fnmatch
+from typing import List, TypeVar
+import os
+
+
+class Auth():
+    """
+    Class to handle user authentication
+    """
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """Public method for require auth"""
+
+        if path is None:
+            return True
+        elif excluded_paths is None or excluded_paths == []:
+            return True
+        else:
+            path = path.rstrip('/')
+            for excluded_path in excluded_paths:
+                excluded_path = excluded_path.rstrip('/')
+                if fnmatch.fnmatch(path, excluded_path):
+                    return False
+            return True
+
+    def authorization_header(self, request=None) -> str:
+        """A method to authorize the header requests"""
+        if request is None:
+            return None
+        if not request.headers.get('Authorization'):
+            return None
+        return request.headers.get('Authorization')
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """current user method"""
+        return None
+
+    def session_cookie(self, request=None):
+        """
+        Method that returns a cookie value from a request
+        """
+        if request is None:
+            return None
+        user_cookies = os.getenv('SESSION_NAME')
+        return request.cookies.get(user_cookies)
